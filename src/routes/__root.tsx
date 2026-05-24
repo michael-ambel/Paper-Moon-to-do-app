77a6a7d7d0e4294c7743acca393417c11dc09fb4
Pull Sync Test <michaelambel46@gmail.com>
2026-05-24 13:59:21 +0300

    test: pull-sync round 2 (modify badge, delete carousel, add footer)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { ThemeProvider } from '../lib/theme'
 import { PullTestBadge } from '../components/PullTestBadge'
+import { PullTestFooter } from '../components/PullTestFooter'
 
 export function Root() {
   return (
@@ -8,6 +9,7 @@ export function Root() {
       <div className="min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 transition-colors">
         <Outlet />
         <PullTestBadge />
+        <PullTestFooter />
       </div>
     </ThemeProvider>
   )
